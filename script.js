9cc0b2cc5e3f24fb3232d13f1e5d8c86de19bf52
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-10-12 19:26:46 +0100

    layout sorted and missing 4 button added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const calcButton = document.querySelectorAll('.calcButton');
+const calcButton = document.querySelector('#calcButtons');
 calcButton.style.gridTemplateColumns = "repeat(4, 1fr)";
 calcButton.style.gridTemplateRows = "repeat(4, 1fr)";
+
+console.log(3 + 5);
