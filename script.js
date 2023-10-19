5c584da684d9e08a148c66ddd8e55ae17b76c8d4
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-10-19 17:30:39 +0100

    new method to calculate using parseFloat and switch statement

--- script.js
+++ script.js
@@ -1,156 +1,115 @@
-let digit = '';
-let digitStored = 0;
-let lastOperator = '';
-let calculation = [];
-let calculated = 0;
+const numberBtn = document.querySelectorAll('[data-number]');
+const operateBtn = document.querySelectorAll('[data-operate]');
+const equalBtn = document.querySelector('[data-equals]');
+const delBtn = document.querySelector('[data-delete]');
+const clearBtn = document.querySelector('[data-clear]');
+const previousText = document.querySelector('[data-previous]');
+const currentText = document.querySelector('[data-current]');
 
-//reset functions - can be removed 
-function resetDigit(){
-    digit = '';
+
+class Calculator{
+    constructor(previousText, currentText){
+        this.previousText = previousText;
+        this.currentText = currentText;
+        this.clear();
+    };
+
+    //non-num button functions
+    clear(){
+        this.previous = '';
+        this.current = '';
+        this.operation = undefined;
+    };
+
+    delete(){
+        this.current = this.current.toString().slice(0,-1);
+    };
+
+    displayNumber(number){
+        if (number === '.' && this.current.includes('.')){
+            return;
+        };
+        this.current = this.current.toString() + number.toString();
+    };
+
+    operate(operation){
+        if (this.current ===''){
+            return;
+        } else if (this.previous !== ''){
+            this.calculate();
+        };
+        this.operation = operation;
+        this.previous = this.current;
+        this.current = '';
+    };
+
+    calculate(){
+        let calculation;
+        const previousNum = parseFloat(this.previous);
+        const currentNum = parseFloat(this.current);
+        if (isNaN(previousNum) || isNaN(currentNum)){
+            return;
+        };
+        switch (this.operation){
+            case '+':
+                calculation = previousNum + currentNum;
+                break;
+            case 'x':
+                calculation = previousNum * currentNum;
+                break;
+            case '-':
+                calculation = previousNum - currentNum;
+                break;
+            case '/':
+                calculation = previousNum / currentNum;
+                break;
+            default:
+                return;
+        };
+        this.current = calculation;
+        this.previous = ''
+        this.operation = undefined;
+    };
+
+    displayNew(){
+        this.currentText.innerText = this.current;
+        if(this.operation != null){
+            this.previousText.innerText = `${this.previous} ${this.operation}`;    
+        } else {
+        this.previousText.innerText = this.previous;
+        };
+    };
+
 };
-function resetDigitStored(){
-    digitStored = 0;
-};
-function resetCalculation(){
-    calculation = [];
-};
-function resetCalculated(){
-    calculated = 0;
-};
 
-// display screen with updating function - currently constantly set to 0 so no use
-const calcScreen = document.querySelector('#current');
-calcScreen.textContent = (`${digit}`);
+const calculator = new Calculator(previous, current);
 
-const calcButtons = document.querySelector('#calcBody');
-calcButtons.addEventListener('mouseup', () => {
-    calcScreen.textContent = (`${digit}`);
+//operation click updates
+clearBtn.addEventListener('click', () => {
+    calculator.clear();
+    calculator.displayNew();
 });
 
-
-//number buttons
-// look in to way to parse element as number, will reduce all functions to 1
-// appendNumber(button.textContent)
-one.addEventListener('mousedown', () => {
-    calculation.push(1);
-    digit += 1;
-});
-two.addEventListener('mousedown', () => {
-    calculation.push(2);
-    digit += 2;
-});
-three.addEventListener('mousedown', () => {
-    calculation.push(3);
-    digit += 3;
-});
-four.addEventListener('mousedown', () => {
-    calculation.push(4);
-    digit += 4;
-});
-five.addEventListener('mousedown', () => {
-    calculation.push(5);
-    digit += 5;
-});
-six.addEventListener('mousedown', () => {
-    calculation.push(6);
-    digit += 6;
-});
-seven.addEventListener('mousedown', () => {
-    calculation.push(7);
-    digit += 7;
-});
-eight.addEventListener('mousedown', () => {
-    calculation.push(8);
-    digit += 8;
-});
-nine.addEventListener('mousedown', () => {
-    calculation.push(9);
-    digit += 9;
-});
-zero.addEventListener('mousedown', () => {
-    calculation.push(0);
-    digit += 0;
+delBtn.addEventListener('click', () => {
+    calculator.delete();
+    calculator.displayNew();
 });
 
-decimal.addEventListener('mousedown', () => {
-    calculation.push('.');
-    digit += '.';
-});
-// start.addEventListener('mousedown', () => {
-//     calculation.push('(');
-//     digit += '(';
-// });
-// finish.addEventListener('mousedown', () => {
-//     calculation.push(')');
-//     digit += ')';
-// });    work just using finish button for other function atm
-
-
-finish.addEventListener('mousedown', () => {
-    console.log(calculation);
-}); 
-
-// store digit on screen as number
-//display it on top of screen digit stored
-
-plus.addEventListener('mousedown', () =>{
-    calculation.push(' + ');
-    resetDigit();
-    console.log(digit);
-    console.log(digitStored);
-    return lastOperater = '+';
-});
-times.addEventListener('mousedown', () =>{
-    calculation.push(' * ');
-    resetDigit();
-    console.log(digit);
-    console.log(digitStored);
-    return lastOperater = '*';
-});
-divide.addEventListener('mousedown', () =>{
-    calculation.push(' / ');
-    resetDigit();
-    console.log(digit);
-    console.log(digitStored);
-    return lastOperater = '/';
-});
-minus.addEventListener('mousedown', () =>{
-    calculation.push(' - ');
-    resetDigit();
-    console.log(digit);
-    console.log(digitStored);
-    return lastOperater = '-';
-});
-//only displays on mousedown, sets to 0 on mouse up atm
-equalButton.addEventListener('mousedown', () =>{
-    calculated = (calculation.join(''));
-    console.log(calculated);
+numberBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.displayNumber(button.innerText);
+        calculator.displayNew();
+    })
 });
 
-clear.addEventListener('mousedown', () => {
-    resetDigit();
-    resetDigitStored();
-    resetCalculation();
-    resetCalculated();
-    console.log(digit);
-    console.log(digitStored);
+operateBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.operate(button.innerText);
+        calculator.displayNew();
+    })
 });
 
-
-// if + - / * return else loops thorugh array to return up top, also break on length
-//set max numbers on screen by rounding if.length> certain amount
-
-// function storeDigitAdd(){
-//     return digitStored = Number(digit) + Number(`${digitStored}`);
-// };
-// //needs fix wrong sequence
-// function storeDigitMinus(){
-//     return digitStored = Number(digit) - Number(`${digitStored}`);
-// };
-// function storeDigitTimes(){
-//     return digitStored = Number(digit) * Number(`${digitStored}`);
-// };
-// //needs fix divide by zero
-// function storeDigitDivide(){
-//     return digitStored = Number(digit) / Number(`${digitStored}`);
-// };
+equalBtn.addEventListener('click', () => {
+    calculator.calculate();
+    calculator.displayNew();
+});
